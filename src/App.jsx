e7c2f48498850a79b93945499a626ae47a979b17
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-02-10 23:44:23 +0600

    Weather-dashboard-htmltoReact

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./component/Header/Header";
+import WeatherBoard from "./component/Weather/WeatherBoard";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <main className="my-[150px]">
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
     </div>
   );
 }
