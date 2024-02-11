6ed89879cff55097f1ca047160711c541fe75519
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-02-12 01:19:03 +0600

    Favourite-contex-provider-local-setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import Header from "./component/Header/Header";
 import WeatherBoard from "./component/Weather/WeatherBoard";
+import FavouriteProvider from "./provider/FavouriteProvider";
 import WeatherProvider from "./provider/WeatherProvider";
 
 export default function App() {
   return (
     <>
       <WeatherProvider>
-        <div className="grid place-items-center h-screen mt-20">
-          <Header />
-          <main>
-            <section>
-              <WeatherBoard />
-            </section>
-          </main>
-        </div>
+        <FavouriteProvider>
+          <div className="grid place-items-center h-screen mt-20">
+            <Header />
+            <main>
+              <section>
+                <WeatherBoard />
+              </section>
+            </main>
+          </div>
+        </FavouriteProvider>
       </WeatherProvider>
     </>
   );
